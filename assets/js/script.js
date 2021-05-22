a4a47344c41a12c1e513068b5d1bd739b9d25a31
pedrazajulian <82198241+pedrazajulian@users.noreply.github.com>
2021-05-22 19:51:48 -0400

    initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,154 @@
+     
+var questions = [{
+    title: "______ JavaScript is also called client-side JavaScript",
+    choices: ["Microsoft", "Navigator", "LiveWire", "Native"],
+    answer: "Navigator"
+},
+{
+    title: "Which of the following are capabilities of functions in JavaScript?",
+    choices: ["Return a value", "Accept parameters and Return a value", "Accept parameters", "None of the above"],
+    answer: "Accept parameters"
+},
+{
+    title: " Which built-in method returns the characters in a string beginning at the specified location?",
+    choices: ["substr", "getSubstring", "slice", "None of the above."],
+    answer: "substr"
+},
+{
+    title: "Which of the following function of an array object adds and/or removes elements from an array?",
+    choices: ["toSource", "sort", "unshift", "splice"],
+    answer: "splice"
+},
+{
+    title: "Choose the client-side JavaScript object:",
+    choices: ["Database", "Cursor", " Client", "FileUpLoad"],
+    answer: "FileUpLoad"
+}
+]
+
+//setting the numerical variables for the functions.. scores and timers.. 
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+//starts the countdown timer once user clicks the 'start' button
+function start() {
+
+timeLeft = 75;
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+timer = setInterval(function() {
+    timeLeft--;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+    //proceed to end the game function when timer is below 0 at any time
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        endGame(); 
+    }
+}, 1000);
+
+next();
+}
+
+//stop the timer
+function endGame() {
+clearInterval(timer);
+
+var quizContent = `
+<h2>Game over!</h2>
+<h3>You got a ` + score +  ` /100!</h3>
+<h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+<input type="text" id="name" placeholder="First name"> 
+<button onclick="setScore()">Set score!</button>`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//store the scores on local storage
+function setScore() {
+localStorage.setItem("highscore", score);
+localStorage.setItem("highscoreName",  document.getElementById('name').value);
+getScore();
+}
+
+
+function getScore() {
+var quizContent = `
+<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h1>` + localStorage.getItem("highscore") + `</h1><br> 
+
+<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+
+`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//clears the score name and value in the local storage if the user selects 'clear score'
+function clearScore() {
+localStorage.setItem("highscore", "");
+localStorage.setItem("highscoreName",  "");
+
+resetGame();
+}
+
+//reset the game 
+function resetGame() {
+clearInterval(timer);
+score = 0;
+currentQuestion = -1;
+timeLeft = 0;
+timer = null;
+
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+var quizContent = `
+<h1>
+    You think you know Javascript?
+</h1>
+<h3>
+    Test Your Knowledge   
+</h3>
+<button onclick="start()">Start!</button>`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//deduct 10seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+timeLeft -= 10; 
+next();
+}
+
+//increases the score by 20points if the user chooses the correct answer
+function correct() {
+score += 20;
+next();
+}
+
+//loops through the questions 
+function next() {
+currentQuestion++;
+
+if (currentQuestion > questions.length - 1) {
+    endGame();
+    return;
+}
+
+var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+    var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+    if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+        buttonCode = buttonCode.replace("[ANS]", "correct()");
+    } else {
+        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+    }
+    quizContent += buttonCode
+}
+
+
+document.getElementById("quizBody").innerHTML = quizContent;
+}
